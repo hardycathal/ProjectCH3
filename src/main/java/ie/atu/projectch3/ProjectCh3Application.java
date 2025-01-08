d8419dbb7c720a70d7e994f031dcec010cfb603f
hardycathal <hardyfcathal@gmail.com>
2025-01-08 18:05:21 +0000

    Created order service

---
+++ src/main/java/ie/atu/projectch3/ProjectCh3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.projectch3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCh3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCh3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/projectch3/ProjectCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectch3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCh3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
